8b1bba3b4020f45f1c6c04cdaa7d697783bc5a9b
swap0308 <swapnildewangan32@gmail.com>
2025-08-06 21:51:45 +0530

    Added Backend Files

---
+++ MusicAppBackend/MusicAppBackend/src/test/java/com/musicappbackend/MusicAppBackend/MusicAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicappbackend.MusicAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
